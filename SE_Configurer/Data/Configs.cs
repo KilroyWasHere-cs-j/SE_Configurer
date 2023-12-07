cfec01cdcc173dd35e5e70fcb6641fd3212092c5
Gabriel Tower <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2023-12-06 22:05:28 -0500

    Basically wrote the app

---
+++ SE_Configurer/Data/Configs.cs
@@ -0,0 +1,8 @@
+﻿namespace SE_Configurer.Data
+{
+    public class Configs
+    {
+        public string Title { get; set; }
+        public Trackable[] Trackables { get; set; }
+    }
+}
---
+++ SE_Configurer/Data/Trackable.cs
@@ -0,0 +1,9 @@
+﻿namespace SE_Configurer.Data
+{
+    public class Trackable
+    {
+        public string Title { get; set; }
+        public string[] Items { get; set; }
+        public string ItemsHold { get; set; }
+    }
+}
